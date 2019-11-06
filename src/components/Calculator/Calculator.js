cffbebad051d52bd9a05f3bafd55414d9111e6ff
Daniel Moffitt <Danielmoffitt54@gmail.com>
2019-11-06 13:42:21 -0600

    Added in Calculator component and styled with sass

---
+++ src/components/Calculator/Calculator.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Calculator.scss';
+
+class Calculator extends Component {
+    render() {
+        return(
+            <div className='Calculator'>
+            </div>
+        );
+    }
+}
+
+export default Calculator;
