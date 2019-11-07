0c025162d9d42dbe31d88f8e9d1f0c8163298ab8
Daniel Moffitt <Danielmoffitt54@gmail.com>
2019-11-07 14:51:46 -0600

    Created Button component and added styling to component.

---
+++ src/components/Button/Button.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './Button.scss';
+
+class Button extends Component {
+    static defaultProps = {
+        type: ''
+    };
+
+    render() {
+        let classes = 'Button';
+
+        if (this.props.type !== '') {
+            classes = 'Button-' + this.props.type;
+        }
+
+        return(
+            <button className={classes}>
+                {this.props.children}
+            </button>
+        );
+    }
+}
+
+export default Button;
--- src/components/Keypad/Keypad.js
+++ src/components/Keypad/Keypad.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
 import './Keypad.scss';
 import KeypadRow from '../KeypadRow/KeypadRow';
+import Button from '../Button/Button';
 
 class Keypad extends Component {
     render() {
         return(
             <div className='Keypad'>
-                <KeypadRow></KeypadRow>
-                <KeypadRow></KeypadRow>
-                <KeypadRow></KeypadRow>
-                <KeypadRow></KeypadRow>
-                <KeypadRow></KeypadRow>
+                <KeypadRow>
+                    <Button type='primary'>C</Button>
+                    <Button type='primary'>&larr;</Button>
+                    <Button type='operator'>%</Button>
+                    <Button type='operator'>/</Button>
+                </KeypadRow>
+                <KeypadRow>
+                    <Button type='value'>7</Button>
+                    <Button type='value'>8</Button>
+                    <Button type='value'>9</Button>
+                    <Button type='operator'>*</Button>
+                </KeypadRow>
+                <KeypadRow>
+                    <Button type='value'>4</Button>
+                    <Button type='value'>5</Button>
+                    <Button type='value'>6</Button>
+                    <Button type='operator'>-</Button>
+                </KeypadRow>
+                <KeypadRow>
+                    <Button type='value'>1</Button>
+                    <Button type='value'>2</Button>
+                    <Button type='value'>3</Button>
+                    <Button type='operator'>+</Button>
+                </KeypadRow>
+                <KeypadRow>
+                    <Button type='value'>0</Button>
+                    <Button type='value'>.</Button>
+                    <Button type='submit'>=</Button>
+                </KeypadRow>
             </div>
         );
     }
