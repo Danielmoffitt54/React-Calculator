2261a87da82006eee7563dff1f2f58f756920275
Daniel Moffitt <Danielmoffitt54@gmail.com>
2019-11-07 13:13:43 -0600

    Created mixins using sass for repeated rules made for components.

---
+++ src/components/Keypad/Keypad.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Keypad.scss';
+
+class Keypad extends Component {
+    render() {
+        return(
+            <div className='Keypad'>
+            </div>
+        );
+    }
+}
+
+export default Keypad;
